1806a7dd93ee4665564f2b711a78f7d2891f23f4
Matthew Russo <matthew@matthewclayrusso.com>
2020-03-23 16:29:29 -0700

    starts to add xr session init code. still need to get raw vulkan pointers

--- src/xr/xr.rs
+++ src/xr/xr.rs
@@ -1,5 +1,17 @@
-use openxr::{ApplicationInfo, Entry, FormFactor, Instance, SystemId};
+use openxr::{
+    ApplicationInfo,
+    Entry,
+    FormFactor,
+    FrameWaiter,
+    FrameStream,
+    Instance,
+    SystemId,
+    Session,
+    Vulkan,
+    Result as OpenXrResult,
+};
 use std::path::Path;
+use openxr::vulkan::SessionCreateInfo;
 
 pub(crate) struct Xr {
     instance: Instance,
@@ -40,6 +52,44 @@ impl Xr {
         Self {
             instance,
             system_id
+        }
+    }
+
+    pub unsafe fn create_vulkan_session(&self, session_create_info: VulkanXrSessionCreateInfo) -> OpenXrResult<VulkanXrSession> {
+        let create_info = SessionCreateInfo {
+            instance: session_create_info.instance,
+            physical_device: session_create_info.physical_device,
+            device: session_create_info.device,
+            queue_family_index: session_create_info.queue_family_index,
+            queue_index: session_create_info.queue_index,
+        };
+
+        self.instance
+            .create_session(self.system_id, &create_info)
+            .map( VulkanXrSession::from)
+    }
+}
+
+pub(crate) struct VulkanXrSessionCreateInfo {
+    instance: vk::Instance,
+    physical_device: vk::PhysicalDevice,
+    device: vk::Device,
+    queue_family_index: u32,
+    queue_index: u32,
+}
+
+pub(crate) struct VulkanXrSession {
+    session: Session<Vulkan>,
+    frame_waiter: FrameWaiter,
+    frame_stream: FrameStream<Vulkan>,
+}
+
+impl From<(Session<Vulkan>, FrameWaiter, FrameStream<Vulkan>)> for VulkanXrSession {
+    fn from(i: (Session<Vulkan>, FrameWaiter, FrameStream<Vulkan>)) -> Self {
+        Self {
+            session: i.0,
+            frame_waiter: i.1,
+            frame_stream: i.2,
         }
     }
 }
